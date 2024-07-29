d37701a68cabe3f7ebf2a4d2651305ea84d8d912
stevenisaacgo <stevenisaacgo03@gmail.com>
2024-07-29 19:26:55 +0200

    Add StartBtn component and integrate it into InitialPage

--- src/pages/InitialPage.vue
+++ src/pages/InitialPage.vue
@@ -1,5 +1,19 @@
 <template>
-    <div class="w-[100%] overflow-hidden">
-        <img class="w-full" src="../assets/Tumblr.gif" />
+    <div class="w-full overflow-hidden flex items-center justify-center relative">
+      <img class="w-full" src="../assets/Tumblr.gif" />
+      <div class="absolute inset-0 flex items-center justify-center">
+        <StartBtn />
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script>
+  import StartBtn from '../components/StartBtn.vue';
+  
+  export default {
+    components: {
+      StartBtn
+    }
+  };
+  </script>
+  
