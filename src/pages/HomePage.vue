36d305f8151e52ba3a00eb7d261363a9ee5288d6
Dunawild <marta.vaquero.calvo@gmail.com>
2024-07-30 12:19:18 +0200

    homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full overflow-hidden flex items-center justify-center relative">
-      <img class="w-full" src="/public/img/imgPagePrincipal.jpeg" />
+      <img class="w-full" src="/img/imgPagePrincipal.jpeg" />
       <div class="absolute inset-0 flex items-center justify-center">
         
       </div>
