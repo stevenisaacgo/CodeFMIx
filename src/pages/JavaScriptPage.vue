f712c2f27458cf78b3431bf805b09a81b6856197
Francesca Batista <francesca.francis.batista@gmail.com>
2024-07-30 08:27:51 +0200

    add main structure of js page

--- src/pages/JavaScriptPage.vue
+++ src/pages/JavaScriptPage.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="w-full">
-        <h1>Practice JavaScript</h1>
+    <div class="w-full grid grid-cols-2 h-full">
+        <div class="border-r-[1px] h-full pt-10">Info</div>
+        <div class="border-l-[1px] h-full pt-10">Question</div>
     </div>
 </template>
+
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
