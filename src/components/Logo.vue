883aa849b9948550e88d98ca018541c624543dbe
Dunawild <marta.vaquero.calvo@gmail.com>
2024-08-01 10:53:39 +0200

    Resolve conflicts

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="ml-4 mt-2 mb-2 bg-none grid grid-cols-2 content-center">
-        <div class="size-8">
-            <img class="bg-none" src="/img/logo.png" alt="Logo" />
-        </div>
-        <div class="ml-4 mt-1 text-sm"><p><a href="index.html"></a>CodeFMIx</p></div>
+    <div class="ml-4 mt-2 mb-2 bg-none grid grid-cols-2 content-center ">
+        <div class="size-8"><img class="bg-none" src="/img/logo.png" alt="Logo"/></div>
+        <div class="ml-4 mt-1 text-sm "><p>CodeFMIx</p></div>
     </div>
 </template>
 
 <script>
-export default {
-    name: "LogoComponent",
-
-    data() {
-        // return {
-        // }
-    },
-};
+    export default {
+        name: "LogoComponent",
+        
+        data() {
+            // return {
+            // }
+        }
+    };
 </script>
 
-<style></style>
+<style>
+
+</style>
