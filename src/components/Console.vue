744a9966cc022eddd78340363c181c5f530c59f2
Dunawild <marta.vaquero.calvo@gmail.com>
2024-08-02 13:09:57 +0200

    añadir respuestas aleatorias al message de console

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class=" ">
+    <div class="console-container">
         <p class="mt-10">CONSOLA</p>
 
-        <div class="bg-black text-white p-6 rounded-lg w-full justify-self-center max-w-lg font-mono text-left justify-cente">
+        <div class="console bg-black text-white p-6 rounded-lg w-full max-w-lg font-mono text-left justify-center">
 
                 <div class="flex space-x-2 text-red-500">
                 <div class="w-3 h-3 rounded-full bg-red-500"></div>
@@ -11,7 +11,7 @@
             </div>
             <p class="text-green-400 mt-4">$ npm install CodeFMIx</p>
         
-            <p class="text-white mt-2"> asdf {{ message }}</p>
+            <p class="text-white mt-2">{{ message }}</p>
 
             <p class="text-green-400 mt-2">$</p>
         </div>
@@ -23,12 +23,40 @@ export default {
     name: "ConsoleComponent",
 
     data() {
-        // return {
-        // }
-    },
+        return {
+            message: "",
+            correctResponses: [
+                "¡Bien hecho!",
+                "Crack!",
+                "¡Correcto!",
+                "¡Menuda racha!"
+            ],
+            incorrectResponses: [
+                "Respuesta incorrecta.",
+                "Inténtalo de nuevo.",
+                "Prueba de nuevo, ¡tú puedes!",
+                "Fallaste, ¡prueba otra vez!"
+            ],
+            };
+  },
+
+  
+  methods: {
+    getRandomResponse(isCorrect) {
+      const responses = isCorrect ? this.correctResponses : this.incorrectResponses; // guardamos en responses si ha sido correcta o no
+      const randomIndex = Math.floor(Math.random() * responses.length);
+      return responses[randomIndex];
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
 
+
+.console {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
 </style>
