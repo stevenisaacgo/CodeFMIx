6c018aaa71bc43d53d01bd7da5d7bd6f3e926ee3
Francesca Batista <francesca.francis.batista@gmail.com>
2024-08-04 17:01:32 +0200

    readme and fixed sound

--- src/components/header/SoundButton.vue
+++ src/components/header/SoundButton.vue
@@ -1,44 +1,35 @@
 <template>
-    <img class="w-[42px]" :src="iconUrl" @click="handleClick" />
-
-    
-  <audio ref="soundMusic" src="../../assets/music/sound.mp3" autoplay loop></audio>
- 
+    <div>
+        <img class="w-[42px]" :src="iconUrl" @click="handleClick" />
+        <audio ref="soundMusic" src="/src/assets/music/sound.mp3" loop></audio>
+    </div>
 </template>
 
 <script setup>
-import { MusicNoteIcon } from "@vue-hero-icons/outline";
 import { ref, onMounted } from "vue";
 
 const urlSpeaker = "/src/assets/speaker.png";
 const urlMute = "/src/assets/muted.png";
 const isMuted = ref(false);
 const iconUrl = ref(urlSpeaker);
-const soundMusic = ref(null);//Inicialmente es null y se asigna al elemento audio dentro de onMounted
+const soundMusic = ref(null);
 
 const handleClick = () => {
     isMuted.value = !isMuted.value;
     iconUrl.value = isMuted.value ? urlMute : urlSpeaker;
-   
-   
-    if (soundMusic.value) { //miramos que no sea null
-      if (isMuted.value) { // si le da a mutear, se  pondrá en pausa
+
+    if (isMuted.value) {
         soundMusic.value.pause();
-      } else {
+    } else {
         soundMusic.value.play();
-      }
     }
 };
 
 onMounted(() => {
-  soundMusic.value = document.querySelector("audio");
-  if (!isMuted.value && soundMusic.value) { //si no está muteado y tiene valor
+    soundMusic.value = document.querySelector("audio");
+    // Start playing the audio automatically on mount
     soundMusic.value.play();
-  } 
 });
-
 </script>
 
-
-<style>
-</style>
+<style scoped></style>
