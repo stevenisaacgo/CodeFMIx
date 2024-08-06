752561c45b0c0d00478be8cc0022e0ab077d2510
stevenisaacgo <stevenisaacgo03@gmail.com>
2024-08-06 18:00:18 +0200

    updateNavigation

--- src/components/header/navigation/Navigation.vue
+++ src/components/header/navigation/Navigation.vue
@@ -22,9 +22,9 @@
                         <div class="py-1">
 
                             <MenuItem as="div" v-slot="{ active }" class="relative">
-                            <Menu as="div" class="relative inline-block text-left">
+                            <Menu as="div" class="relative inline-block text-left w-full ">
                                 <MenuButton
-                                    class="inline-flex w-full justify-center gap-x-1.5 bg-[#242424] px-3 py-2 text-sm font-semibold text-[#ffffffde] outline-none hover:border-0 active:border-0">
+                                    class="inline-flex justify-between w-full gap-x-1.5  hover:bg-gray-100 hover:text-gray-900 bg-[#242424] px-3 py-2 text-sm font-semibold text-[#ffffffde] outline-none hover:border-0 active:border-0">
                                     HTML
                                     <ChevronDownIcon class="ml-2 h-5 w-5 text-gray-400 rotate-[270deg]"
                                         aria-hidden="true" />
@@ -75,9 +75,9 @@
                             </Menu>
                             </MenuItem>
                             <MenuItem as="div" v-slot="{ active }" class="relative">
-                            <Menu as="div" class="relative inline-block text-left">
+                            <Menu as="div" class="relative inline-block text-left w-full">
                                 <MenuButton
-                                    class="inline-flex w-full justify-center gap-x-1.5 bg-[#242424] px-3 py-2 text-sm font-semibold text-[#ffffffde] outline-none hover:border-0 active:border-0">
+                                    class="inline-flex w-full justify-between gap-x-1.5 hover:bg-gray-100 hover:text-gray-900 bg-[#242424] px-3 py-2 text-sm font-semibold text-[#ffffffde] outline-none hover:border-0 active:border-0">
                                     CSS
                                     <ChevronDownIcon class="ml-2 h-5 w-5 text-gray-400 rotate-[270deg]"
                                         aria-hidden="true" />
@@ -128,9 +128,9 @@
                             </Menu>
                             </MenuItem>
                             <MenuItem as="div" v-slot="{ active }" class="relative">
-                            <Menu as="div" class="relative inline-block text-left">
+                            <Menu as="div" class="relative inline-block text-left w-full">
                                 <MenuButton
-                                    class="inline-flex w-full justify-center gap-x-1.5 bg-[#242424] px-3 py-2 text-sm font-semibold text-[#ffffffde] outline-none hover:border-0 active:border-0">
+                                    class="inline-flex w-full justify-between gap-x-1.5 hover:bg-gray-100 hover:text-gray-900 bg-[#242424] px-3 py-2 text-sm font-semibold text-[#ffffffde] outline-none hover:border-0 active:border-0">
                                     JavaScript
                                     <ChevronDownIcon class="ml-2 h-5 w-5 text-gray-400 rotate-[270deg]"
                                         aria-hidden="true" />
